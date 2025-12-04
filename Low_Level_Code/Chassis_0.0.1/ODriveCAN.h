802f9355b50845dc6010d4add6a470286724b239
matthew1775 <amatvey62@gmail.com>
2025-12-04 15:06:03 +0100

    zmiany arduino error

---
+++ Low_Level_Code/Chassis_0.0.1/ODriveCAN.h
@@ -0,0 +1,38 @@
+// ODriveCAN.h
+#ifndef ODRIVE_CAN_H
+#define ODRIVE_CAN_H
+
+#include <Arduino.h>
+#include <CAN.h>
+#include "Config.h"
+
+// Komendy ODrive
+#define CMD_ID_GET_ERROR       0x003
+#define CMD_ID_SET_AXIS_STATE  0x007
+#define CMD_ID_GET_ENCODER     0x009
+#define CMD_ID_SET_CONTROLLER_MODE 0x00B
+#define CMD_ID_SET_INPUT_VEL   0x00D
+#define CMD_ID_REBOOT_ODRIVE   0x016 
+#define CMD_ID_CLEAR_ERRORS    0x018
+
+#define AXIS_STATE_ENCODER_OFFSET_CALIBRATION 7
+#define AXIS_STATE_CLOSED_LOOP_CONTROL        8
+#define CONTROL_MODE_VELOCITY_CONTROL         2
+#define INPUT_MODE_PASSTHROUGH                1
+#define INPUT_MODE_VEL_RAMP                   2  
+
+
+void initCAN();
+void sendVelocity(float velocity);
+void setAxisState(int32_t state);
+void setControlMode(int32_t controlMode, int32_t inputMode);
+void requestODriveErrors();
+void requestEncoderData();
+void clearErrors();
+void rebootODrive();
+void handleCANMessages(); 
+
+
+
+
+#endif
